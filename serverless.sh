435e91def7929ddd3362fcf50ee00222048bad39
Ash Jindal <ajorpheus@users.noreply.github.com>
2020-01-17 10:52:27 +0000

    Fix OOM error
    This may fix the OOM error seen on the CI/CD env:
    
    "FATAL ERROR: Ineffective mark-compacts near heap 
    limit Allocation failed - JavaScript heap out of memory"

--- serverless.sh
+++ serverless.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+export NODE_OPTIONS=--max_old_space_size=8192
+
 cd disclosure-deploy-repository
 echo Installing dependancies
 serverless plugin install --name serverless-latest-layer-version
