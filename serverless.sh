2750930e581bedefea4273d8c0ea1435cf0b2622
Jordancooke <Jordanleecooke@gmail.com>
2020-01-20 14:31:11 +0000

    Removing memory increase for node and using latest version

--- serverless.sh
+++ serverless.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
-export NODE_OPTIONS=--max_old_space_size=8192
-
 cd disclosure-deploy-repository
 echo Installing dependancies
-npm i --only=production serverless-latest-layer-version@2.0.0
+serverless plugin install --name serverless-latest-layer-version
 echo Packaging serverless bundle...
 serverless package --package pkg
 echo Deploying to AWS...
