e57104b84069a88a458643927d08e23a2e7dfb1c
Jordancooke <Jordanleecooke@gmail.com>
2020-01-20 13:37:53 +0000

    Rolling back version and adding --only

--- serverless.sh
+++ serverless.sh
@@ -2,7 +2,7 @@
 
 cd disclosure-deploy-repository
 echo Installing dependancies
-npm i serverless-latest-layer-version
+npm i --only=production serverless-latest-layer-version@2.0.0
 echo Packaging serverless bundle...
 serverless package --package pkg
 echo Deploying to AWS...
