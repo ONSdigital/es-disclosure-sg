2921889acb3d4477ac4a4363811e9647e749fedd
Jordancooke <Jordanleecooke@gmail.com>
2020-01-20 13:21:47 +0000

    Changing plugin install to npm in serverless.sh

--- serverless.sh
+++ serverless.sh
@@ -2,7 +2,7 @@
 
 cd disclosure-deploy-repository
 echo Installing dependancies
-serverless plugin install --name serverless-latest-layer-version@2.0.0
+npm i serverless-latest-layer-version
 echo Packaging serverless bundle...
 serverless package --package pkg
 echo Deploying to AWS...
