528ecd8c99b4b07f35868cedaa1b2cae07e4c4f6
Jordancooke <Jordanleecooke@gmail.com>
2020-01-20 12:43:15 +0000

    Removing memory increase for node and back-dating latest layer plugin

--- serverless.sh
+++ serverless.sh
@@ -2,7 +2,7 @@
 
 cd disclosure-deploy-repository
 echo Installing dependancies
-serverless plugin install --name serverless-latest-layer-version
+serverless plugin install --name serverless-latest-layer-version@2.0.0
 echo Packaging serverless bundle...
 serverless package --package pkg
 echo Deploying to AWS...
