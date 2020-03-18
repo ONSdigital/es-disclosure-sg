2370fcf3e5124ce5415f80b409e7868f525e5f7f
KrisRogos <krisrogos@gmail.com>
2020-03-18 11:31:53 +0000

    ,

--- tests/test_disclosure_wrangler.py
+++ tests/test_disclosure_wrangler.py
@@ -37,7 +37,7 @@ mock_wrangles_event = {
     "unique_identifier": ["responder_id"],
     "in_file_name": "testing",
     "incoming_message_group_id": "testing",
-    "location": "Here"
+    "location": "Here",
     "sns_topic_arn": "testing"
   }
 }
@@ -51,7 +51,7 @@ class TestWrangler(unittest.TestCase):
             {
                 "checkpoint": "testing",
                 "bucket_name": "testing",
-                "method_name": "es-disclosure-stage--method",
+                "method_name": "es-disclosure-stage--method"
             }
         )
 
